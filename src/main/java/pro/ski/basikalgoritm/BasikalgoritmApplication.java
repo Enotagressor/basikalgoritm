31a554d61915dbc7586c51e464a0bf07dd202734
Sergey <enotagressor@gmail.com>
2022-08-10 00:24:43 +0700

    1

---
+++ src/main/java/pro/ski/basikalgoritm/BasikalgoritmApplication.java
@@ -0,0 +1,13 @@
+package pro.ski.basikalgoritm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasikalgoritmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasikalgoritmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/ski/basikalgoritm/BasikalgoritmApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.ski.basikalgoritm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasikalgoritmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
